f36cdec07aec2ea2069139c7accbe6786256668f
sxit_leiyan <524297115@qq.com>
2017-01-03 16:23:31 +0800

    ..

---
+++ 随便练练手/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  随便练练手
+//
+//  Created by apple on 17/1/3.
+//  Copyright © 2017年 雷晏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
